d42d444bd05ebb6c8f82eea2cbb58e0cf5913b1a
Valeco96 <correalev2@gmail.com>
2025-04-11 00:49:49 +0200

    Completed the HTML structure and worked on almost the JS functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Il tuo compito è creare un sito e-commerce per Smartphones. 
-// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
+// Il tuo compito è creare un sito e-commerce per Smartphones.
+// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto.
 // Non sono richieste funzionalità di carrello/cassa.
 // - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
 // - Completa gli altri esercizi con JS
@@ -15,10 +15,33 @@
 
 //ESERCIZI IN JS
 // ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+
+function cambiaTitolo() {
+  let heading = document.querySelector("h1");
+  heading.textContent = "🎁 Silly gifts for silly people 🎁";
+}
+
 // ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+
+function cambiaTema() {
+  let body = document.querySelector("body");
+  body.classList.add("ligther");
+}
+
 // ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+
+function cambiaIndirizzo() {
+  let address = document.getElementById("indirizzo");
+  address.innerHTML = "Amsterdamsestraatweg 55B, 3512TB, Utrecht";
+}
+
 // ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+
+function cambiaColoreLink() {
+  let linkColor = document.getElementsByClassName("link");
+  linkColor.classList.add("link-color");
+}
+
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+
 // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
-
-
